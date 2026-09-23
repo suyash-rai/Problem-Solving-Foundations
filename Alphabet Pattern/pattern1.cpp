045058156c72c6dbf68b7e50b0bcb96d8c27b08f
Suyash Rai <raisuyash.a15@gmail.com>
2026-09-23 20:39:49 +0530

    Add alphabet pattern 1

---
+++ Alphabet Pattern/pattern1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int rows){
+    for (int i = 0 ; i < rows; i++)               
+    {                                            
+        for (char ch = 'A'; ch <= 'A' + i; ch++) 
+        {                                        
+            cout << ch;                          
+        }
+        cout << endl; 
+    }
+}
+
+int main(){
+    int rows;
+    cout<<"How many rows do you want ?"<<endl;
+    cin>>rows;
+    pattern1(rows);
+}
+// Pattern :-
+//  A
+//  AB
+//  ABC
+//  ABCD
+//  ABCDE
